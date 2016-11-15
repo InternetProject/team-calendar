cd1c4d95c2b10ce64e4c2179a4a79d97841938af
QQ <qlin@wesleyan.edu>
2016-11-14 22:02:49 -0800

    Base to work from

---
+++ Calendar/Calendar/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+    }
+}
---
+++ Calendar/Calendar/Models/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Calendar.Models
+{
+    public class Event
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public string Location { get; set; }
+    }
+
+    public class EventDBContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+    }
+}
